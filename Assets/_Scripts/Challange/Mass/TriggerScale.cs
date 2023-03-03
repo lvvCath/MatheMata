75b3e5bc79e0d90f138ef5e8efcbc64fe90acc47
eriasaru <58615074+EMCabarles@users.noreply.github.com>
2023-03-03 23:33:30 +0800

    Updates on Mass Hard Difficulty
    - Added Extra assets for Objects used in the Mass Hard Difficulty
    
    - Added Prefabs (Heavy Objects and Light Objects)
    
    - Added Collision on Objects at prefab

--- Assets/_Scripts/Challange/Mass/TriggerScale.cs
+++ Assets/_Scripts/Challange/Mass/TriggerScale.cs
@@ -4,15 +4,90 @@ using UnityEngine;
 
 public class TriggerScale : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Scale")]
+    public GameObject scaleBar;
+    public GameObject LeftScale;
+    public float rotateTime;
+
+    [Header("Question")]
+    public GameObject Q4;
+    public GameObject Q5;
+
+    private GameObject objDetected;
+    private float objMass;
+
+    private float scaleRight_totalMass;
+    private float scaleLeft_mass;
+
+    private bool isEqual;
+
+    private void OnEnable()
     {
-        
+        // if Q4 is active
+        if (Q4.activeSelf)
+        {
+            scaleRight_totalMass = 0;
+            scaleLeft_mass = LeftScale.GetComponent<LeftScale>().GetWatermelonMass();
+        }
+
+        if (Q5.activeSelf)
+        {
+            scaleRight_totalMass = 0;
+            scaleLeft_mass = LeftScale.GetComponent<LeftScale>().GetPineappleMass();
+        }
+
+        scale();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public float GetTotalMass()
     {
-        
+        return scaleRight_totalMass;
+    }
+
+    public bool IsEqual()
+    {
+        return isEqual;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        objDetected = collision.gameObject;
+        objMass = objDetected.GetComponent<Rigidbody2D>().mass;
+
+        scaleRight_totalMass += objMass;
+
+        scale();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        objDetected = collision.gameObject;
+        objMass = objDetected.GetComponent<Rigidbody2D>().mass;
+
+        scaleRight_totalMass -= objMass;
+
+        scale();
+    }
+
+    void scale()
+    {
+        if (scaleLeft_mass == scaleRight_totalMass) // 0
+        {
+            isEqual = true;
+            LeanTween.rotateZ(scaleBar, 0, rotateTime);
+        }
+
+        if (scaleLeft_mass < scaleRight_totalMass) // -10
+        {
+            isEqual = false;
+            LeanTween.rotateZ(scaleBar, -10, rotateTime);
+        }
+
+        if (scaleLeft_mass > scaleRight_totalMass) // 10
+        {
+            isEqual = false;
+            LeanTween.rotateZ(scaleBar, 10, rotateTime);
+        }
     }
 }
